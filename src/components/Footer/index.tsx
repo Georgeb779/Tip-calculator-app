06fc97ac63248af6230c765ca216013c4010513a
George Baez <g.baez@gbh.com.do>
2022-08-26 09:46:15 -0400

    Add Desktop version

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,23 +3,26 @@ import "./style.scss";
 export function Footer() {
   return (
     <footer className='footer__container'>
-      Challenge by
-      <a
-        target='_blank'
-        rel='noopener noreferrer'
-        href='https://www.frontendmentor.io/challenges/rock-paper-scissors-game-pTgwgvgH'
-      >
-        Frontend Mentor
-      </a>
-      Coded by
-      <a
-        target='_blank'
-        rel='noopener noreferrer'
-        href='https://github.com/Georgeb779'
-      >
-        Georgeb779
-      </a>
-      .
+      <div>
+        {" "}
+        Challenge by
+        <a
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://www.frontendmentor.io/challenges/rock-paper-scissors-game-pTgwgvgH'
+        >
+          Frontend Mentor
+        </a>
+        Coded by
+        <a
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://github.com/Georgeb779'
+        >
+          Georgeb779
+        </a>
+        .
+      </div>
     </footer>
   );
 }
--- src/components/Result/index.tsx
+++ src/components/Result/index.tsx
@@ -5,8 +5,11 @@ import "./style.scss";
 export function Result() {
   return (
     <div className='result__container'>
-      <TipValue text={"Tip Amount"} value={4.27} />
-      <TipValue text={"Total"} value={32.79} />
+      <div className='result__container-text'>
+        <TipValue text={"Tip Amount"} value={100000} />
+        <TipValue text={"Total"} value={1000000} />
+      </div>
+
       <Button
         type='secondary'
         text={"RESET"}
