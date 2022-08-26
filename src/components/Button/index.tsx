1ccca93bcda5a88a153a3d2045bec7918c1b8b6e
George Baez <g.baez@gbh.com.do>
2022-08-25 20:11:40 -0400

    Create and style Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { ButtonProps } from "../../interfaces";
+import "./style.scss";
 
-export function Button() {
-  return <div>Button</div>;
+export function Button({ text, onClick, type }: ButtonProps) {
+  return (
+    <div className='button__container'>
+      <button className={type} onClick={onClick}>
+        {text}
+      </button>
+    </div>
+  );
 }
