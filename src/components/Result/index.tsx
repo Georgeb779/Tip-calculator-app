886df3f109336398983a723e59c2cdfbc24b598a
George Baez <g.baez@gbh.com.do>
2022-08-25 20:12:38 -0400

    Create and style Result component

--- src/components/Result/index.tsx
+++ src/components/Result/index.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Button, TipValue } from "../";
+import "./style.scss";
 
-export  function Result() {
+export function Result() {
   return (
-    <div>index</div>
-  )
+    <div className='result__container'>
+      <TipValue text={"Tip Amount"} value={4.27} />
+      <TipValue text={"Total"} value={32.79} />
+      <Button
+        type='secondary'
+        text={"RESET"}
+        onClick={function (): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
+    </div>
+  );
 }
