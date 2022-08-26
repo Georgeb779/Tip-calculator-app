77d59684127b85d92e4ca70a87c28eac130df023
George Baez <g.baez@gbh.com.do>
2022-08-25 20:13:16 -0400

    Create data for tips

---
+++ src/data/tips.ts
@@ -0,0 +1,22 @@
+export const tipsData = [
+  {
+    text: "5%",
+    type: "primary"
+  },
+  {
+    text: "10%",
+    type: "primary"
+  },
+  {
+    text: "15%",
+    type: "primary"
+  },
+  {
+    text: "25%",
+    type: "primary"
+  },
+  {
+    text: "50%",
+    type: "primary"
+  }
+];
