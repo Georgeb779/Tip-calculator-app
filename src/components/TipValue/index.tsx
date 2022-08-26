392db63d08fff5644bc7293314c50f75256e8566
George Baez <g.baez@gbh.com.do>
2022-08-25 20:12:54 -0400

    Create and style TipValue component

---
+++ src/components/TipValue/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./style.scss";
+
+export function TipValue({
+  text,
+  value
+}: {
+  text: number | string;
+  value: number | string;
+}) {
+  return (
+    <div className='tip-value__container'>
+      <div>
+        <h1>{text}</h1>
+        <p>/ person</p>
+      </div>
+      <div>
+        <h2>${value}</h2>
+      </div>
+    </div>
+  );
+}
